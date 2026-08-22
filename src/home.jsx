2ba42bb9eabe1aa1a56a1f2128cbca7c75610e33
velmuruganshanmugam2211 <velmuruganshanmugam2211@gmail.com>
2026-08-22 17:00:08 +0530

    Testing Commit

--- src/home.jsx
+++ src/home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-    <div><h1>Hi How are you</h1></div>
+    <div><h1>Hi How are you, heloooo</h1></div>
       <nav> <div className="flex flex-row gap-5" >
         <Link to="/home">Home</Link>
         <Link to="/counter">Counter</Link>
